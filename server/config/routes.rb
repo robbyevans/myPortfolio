2f908dcc8a8ce0fde7143d6885d6954937b93a09
Robby Evans <ruttoevans001@gmail.com>
2024-11-15 13:52:39 +0300

    updated server routes.rb

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  root 'projects#index' # Sets the root route to the projects index page
+
   resources :projects do
     collection do
       patch :update_order
     end
   end
+
   post '/login', to: 'authentication#login'
   post '/signup', to: 'authentication#signup'
 end
