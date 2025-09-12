3d8ecb153b98c11d754501f40f325e2ca8739af2
Robby Evans <ruttoevans001@gmail.com>
2025-09-12 14:11:24 +0300

    minor fix

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -1,10 +1,9 @@
-# server/config/puma.rb
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5).to_i
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS', max_threads_count).to_i
 threads min_threads_count, max_threads_count
 
+# Listen on Fly’s PORT
 port ENV.fetch('PORT', 8080)
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 8080)}"
 
 environment ENV.fetch('RACK_ENV') { ENV.fetch('RAILS_ENV', 'production') }
 workers ENV.fetch('WEB_CONCURRENCY', 0).to_i
